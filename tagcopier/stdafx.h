8518f6db8dc47e275c71c2b2b12435878656107f
Bernhard Döbler <programmer@bardware.de>
2016-04-03 01:30:44 +0200

    sample code to read tags added

--- tagcopier/stdafx.h
+++ tagcopier/stdafx.h
@@ -7,9 +7,22 @@
 
 #include "targetver.h"
 
+#include <iostream>
+#include <iomanip>
+
 #include <stdio.h>
 #include <tchar.h>
 
-
-
 // TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
+
+#include "fileref.h"
+#include "tag.h"
+#include "toolkit/tpropertymap.h"
+
+#include "mpeg/mpegfile.h"
+#include "mpeg/id3v2/frames/attachedpictureframe.h"
+#include "mpeg/id3v2/id3v2tag.h"
+#include "mp4/mp4file.h"
+#include "mp4/mp4tag.h"
+#include "mp4/mp4coverart.h"
+#include "flac/flacpicture.h"
